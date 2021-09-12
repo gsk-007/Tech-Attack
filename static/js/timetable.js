12a04eed65a1d574ec6ccc2b96117a8c79cb6662
Yash Gupta <76595447+conqryash007@users.noreply.github.com>
2021-09-12 11:25:32 +0530

    added glow effect

--- static/js/timetable.js
+++ static/js/timetable.js
@@ -7,21 +7,21 @@ const thu = document.querySelectorAll(".thu");
 const fri = document.querySelectorAll(".fri");
 // for glow effect on hover
 
-function glow(ele) {
-  ele[0].addEventListener("mouseover", () => {
+function glow(ele, i) {
+  ele[i].addEventListener("mouseover", () => {
     ele.forEach((curr) => {
       curr.classList.toggle("glow");
     });
   });
-  ele[0].addEventListener("mouseout", () => {
+  ele[i].addEventListener("mouseout", () => {
     ele.forEach((curr) => {
       curr.classList.toggle("glow");
     });
   });
 }
 
-glow(mon);
-glow(tue);
-glow(wed);
-glow(thu);
-glow(fri);
+glow(mon, 4);
+glow(tue, 5);
+glow(wed, 5);
+glow(thu, 5);
+glow(fri, 4);
