76a76f297dfb8316fdc7656a620ef85d8ae58f09
Ronny <gsingh16742@gmail.com>
2021-09-13 22:08:14 +0530

    Added To-Do List

---
+++ static/js/to-do.js
@@ -0,0 +1,48 @@
+let myTasks = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tasksFromLocalStorage = JSON.parse( localStorage.getItem("myTasks") )
+// const tabBtn = document.getElementById("tab-btn")
+
+if (tasksFromLocalStorage) {
+    myTasks = tasksFromLocalStorage
+    render(myTasks)
+}
+
+// tabBtn.addEventListener("click", function(){    
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+//         myLeads.push(tabs[0].url)
+//         localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+//         render(myLeads)
+//     })
+// })
+
+function render(tasks) {
+    let listItems = ""
+    for (let i = 0; i < tasks.length; i++) {
+        listItems += `
+            <li>
+                    ${tasks[i]}
+                    
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myTasks = []
+    render(myTasks)
+})
+
+inputBtn.addEventListener("click", function() {
+    if(inputEl.value != ""){
+    // console.log(inputEl.value)
+    myTasks.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myTasks", JSON.stringify(myTasks) )
+    render(myTasks)}
+})
