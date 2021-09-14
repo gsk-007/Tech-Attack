0961d94b18151710825dc3ae02cb066df063fbe1
conqryash007 <guptayash200010@gmail.com>
2021-09-14 14:12:48 +0530

    updating login js

--- static/js/login.js
+++ static/js/login.js
@@ -1,22 +1,19 @@
 "use strict";
 
-const stdInputName = `document.querySelector(".")`;
-const stdInputPass = `document.querySelector(".")`;
+const signupSubmit = document.querySelector();
 
-const newStd = {
-  name: stdInputName,
-  password: stdInputName,
-  roll_no: "B4200XX",
-  image: male_def_img,
-  image_icon: male_def_icon,
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper, lorem quis placerat rutrum, purus mauris tristique sapien, ut rhoncus ante ex a nisi. Nunc tincidunt augue dignissim augue fringilla, ac luctus urna maximus. Proin lobortis tortor et mattis facilisis. Nulla facilisi. Quisque sit amet porttitor nunc, sed fringilla elit.",
-  achivements: [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "Mauris sit amet dolor vitae quam mattis tincidunt in a sem.",
-    "Ut at leo at sem varius venenatis.",
-  ],
-  skills: "1. X | 2. X | 3. X",
-};
-
-// student_info.push(newStd);
+signupSubmit.addEventListener("click", () => {
+  const userName = `document.querySelector(".")`;
+  const userPass = `document.querySelector(".")`;
+  function whichUser(ele) {
+    return ele.name === userName;
+  }
+  currUser = student_info.find(whichUser);
+  if (currUser === undefined) {
+    window.alert("INVALID USER PLEASE LOG IN FIRST OR RECHECK THE FIELDS");
+  } else if (currUser.password !== userPass) {
+    window.alert("WRONG PASSWORD! PLEASE RECHECK YOUR PASSWORD");
+  } else {
+    window.alert(`WELCOME ${currUser.name}`);
+  }
+});
