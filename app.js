479130de8536694a7a5fc01fe466a822a80bfcfe
gsk-007 <81287219+gsk-007@users.noreply.github.com>
2021-09-15 17:57:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 //const bodyparser = require('body-parser')
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use('/static',express.static('static'))
 app.use(express.urlencoded())
