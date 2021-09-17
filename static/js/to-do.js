7390c3fc30571cafbbbf7739394e62f0a14e32ce
Ronny <gsingh16742@gmail.com>
2021-09-17 11:21:03 +0530

    Minor Changes

--- static/js/to-do.js
+++ static/js/to-do.js
@@ -11,13 +11,6 @@ if (tasksFromLocalStorage) {
   render(myTasks);
 }
 
-// tabBtn.addEventListener("click", function(){
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-//         myLeads.push(tabs[0].url)
-//         localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-//         render(myLeads)
-//     })
-// })
 
 function render(tasks) {
   let listItems = "";
