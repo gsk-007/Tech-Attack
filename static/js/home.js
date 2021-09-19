badbbdf4bb8a54d11766d3e3d8f80fae01ae7dc1
conqryash007 <guptayash200010@gmail.com>
2021-09-19 19:36:34 +0530

    Added some responsive styling to pages

--- static/js/home.js
+++ static/js/home.js
@@ -5,12 +5,12 @@
 //   navbar.classList.add('flex-all-center')
 // })
 
-const toggleButton = document.querySelector('.toggle-button')
-const navbarLinks = document.querySelector('.navbar')
-const btnLinks = document.querySelector('.right')
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-//   btnLinks.classList.toggle('active')
-
-})
+const toggleButton = document.querySelector(".toggle-button");
+const navbarLinks = document.querySelector(".navbar");
+const btnLinks = document.querySelector(".right");
+const header = document.querySelector(".header");
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+  //   btnLinks.classList.toggle('active')
+  header.classList.toggle("marg");
+});
