64bee6325a16cbc4fccd06cc93de6e0f61dfeaf9
gsk-007 <81287219+gsk-007@users.noreply.github.com>
2021-09-21 07:54:36 +0530

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -1,16 +1,12 @@
-// const toggleButton = document.querySelector('toggle-button')
-// const navbar = document.getElementsByClassName('navbar')
 
-// toggleButton.addEventListener('click', () => {
-//   navbar.classList.add('flex-all-center')
-// })
 
 const toggleButton = document.querySelector(".toggle-button");
 const navbarLinks = document.querySelector(".navbar");
 const btnLinks = document.querySelector(".right");
 const header = document.querySelector(".header");
+
+// For toggling classes to display navbar
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
-  //   btnLinks.classList.toggle('active')
   header.classList.toggle("marg");
 });
